ad89b31bf671befa4af4b98083d1c70f81acab1c
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-24 17:16:37 -0500

    api fetch returns alert messages for the 3 train

--- server/apiController.js
+++ server/apiController.js
@@ -2,12 +2,14 @@ const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 
 const URL =
   'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm';
+const URLalert =
+  'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts';
 const key = 'IWddVccjk6aIOEuV87rGV8p2551sWLzI3y6O65yu';
 
 const ApiController = {
   async getSubwayInfo(req, res, next) {
     try {
-      response = await fetch(URL, {
+      response = await fetch(URLalert, {
         headers: { 'x-api-key': key },
       });
 
@@ -17,8 +19,11 @@ const ApiController = {
       );
 
       const data = [];
+      // returns all alerts for a given line
       feed.entity.forEach((element) => {
-        if (element.tripUpdate && element.tripUpdate.trip) data.push(element.tripUpdate.trip.routeId);
+        element.alert.informedEntity.forEach(x => {
+          if(x.routeId === '3') data.push(element.alert.headerText.translation[0].text)
+        })
       });
 
       res.locals.data = data;
