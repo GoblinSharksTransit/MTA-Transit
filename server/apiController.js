e64ac200320d19010d0dd3e38da7f35fa24ddb18
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-26 13:50:41 -0500

    refined return data

--- server/apiController.js
+++ server/apiController.js
@@ -18,12 +18,37 @@ const ApiController = {
         new Uint8Array(buffer)
       );
 
-      const data = [];
+      const currentTime = Math.floor(Date.now() / 1000);
+      const route = 'F';
+
+      const data = [route];
+      const cache = {};
       // returns all alerts for a given line
       feed.entity.forEach((element) => {
-        element.alert.informedEntity.forEach(x => {
-          if(x.routeId === '3') data.push(element.alert.headerText.translation[0].text)
-        })
+        element.alert.informedEntity.forEach((x) => {
+          if (x.routeId === route) {
+            element.alert.activePeriod.forEach((y) => {
+              if (y.start.low < currentTime) {
+                if (y.end.low === 0 || y.end.low > currentTime) {
+                const message = element.alert.headerText.translation[0].text;
+                const start = new Date(y.start.low * 1000);
+                const end = new Date(y.end.low * 1000);
+
+                if (!cache[message]) {
+                  const obj = {
+                    message: message,
+                    start: dateString(start),
+                  };
+                  if (y.end.low !== 0) obj.end = dateString(end)
+                  else obj.end = 'Unknown'
+                  data.push(obj);
+                  cache[message] = true;
+                }
+                }
+              }
+            });
+          }
+        });
       });
 
       res.locals.data = data;
@@ -33,5 +58,28 @@ const ApiController = {
     }
   },
 };
+
+function dateString(date) {
+  let string = `${date.getMonth() + 1}/${date.getDate()} `;
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let pm = false;
+
+  if (hours === 12) {
+    pm = true;
+    string += `12:`;
+  } else if (hours > 12) {
+    pm = true;
+    string += `${hours % 12}:`;
+  } else string += `${hours}:`;
+
+  if (minutes < 10) string += `0${minutes}`;
+  else string += `${minutes}`;
+
+  if (pm) string += ' PM';
+  else string += ' AM';
+
+  return string;
+}
 
 module.exports = ApiController;
