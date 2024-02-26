5585aefe5ed57c5dc4e5ebae076be2e492f527cf
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-26 18:52:58 -0500

    for main

--- server/apiController.js
+++ server/apiController.js
@@ -1,13 +1,15 @@
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 
 const URL =
+  'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm';
+const URLalert =
   'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts';
 const key = 'IWddVccjk6aIOEuV87rGV8p2551sWLzI3y6O65yu';
 
 const ApiController = {
   async getSubwayInfo(req, res, next) {
     try {
-      response = await fetch(URL, {
+      response = await fetch(URLalert, {
         headers: { 'x-api-key': key },
       });
 
@@ -17,49 +19,40 @@ const ApiController = {
       );
 
       const currentTime = Math.floor(Date.now() / 1000);
-      const routes = req.body.subways;
-      const data = [];
+      const route = 'F';
 
-      routes.forEach((route) => {
-        const info = [route];
-        const cache = {};
+      const data = [route];
+      const cache = {};
+      // returns all alerts for a given line
+      feed.entity.forEach((element) => {
+        element.alert.informedEntity.forEach((x) => {
+          if (x.routeId === route) {
+            element.alert.activePeriod.forEach((y) => {
+              if (y.start.low < currentTime) {
+                if (y.end.low === 0 || y.end.low > currentTime) {
+                const message = element.alert.headerText.translation[0].text;
+                const start = new Date(y.start.low * 1000);
+                const end = new Date(y.end.low * 1000);
 
-        feed.entity.forEach((element) => {
-          element.alert.informedEntity.forEach((x) => {
-
-            if (x.routeId === route) {
-              element.alert.activePeriod.forEach((y) => {
-
-                if (y.start.low < currentTime) {
-                  if (y.end.low === 0 || y.end.low > currentTime) {
-
-                    const message =
-                      element.alert.headerText.translation[0].text;
-                    const start = new Date(y.start.low * 1000);
-                    const end = new Date(y.end.low * 1000);
-
-                    if (!cache[message]) {
-                      const obj = {
-                        message: message,
-                        start: dateString(start),
-                      };
-                      if (y.end.low !== 0) obj.end = dateString(end);
-                      else obj.end = 'Unknown';
-                      info.push(obj);
-                      cache[message] = true;
-                    }
-                  }
+                if (!cache[message]) {
+                  const obj = {
+                    message: message,
+                    start: dateString(start),
+                  };
+                  if (y.end.low !== 0) obj.end = dateString(end)
+                  else obj.end = 'Unknown'
+                  data.push(obj);
+                  cache[message] = true;
                 }
-              });
-            }
-          });
+                }
+              }
+            });
+          }
         });
-        data.push(info);
       });
 
       res.locals.data = data;
       return next();
-      
     } catch {
       return next((error) => console.log(`Error fetching data`, error));
     }
@@ -68,8 +61,8 @@ const ApiController = {
 
 function dateString(date) {
   let string = `${date.getMonth() + 1}/${date.getDate()} `;
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
   let pm = false;
 
   if (hours === 12) {
@@ -90,3 +83,4 @@ function dateString(date) {
 }
 
 module.exports = ApiController;
+
