335ccb14a1341a0345520472aef4fe8000f69448
d.wong92 <d.wong92@gmail.com>
2024-02-26 10:18:43 -0500

    final component

---
+++ component/SubwayLineComponent.jsx
@@ -0,0 +1,24 @@
+// import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+
+
+const SubwayLineComponent = () => {
+    return (
+        <div className="totalBox">
+            <div className="subwayLine">
+                <p>2</p>
+                <span id="subwayLine"></span>
+            </div>
+            <div className="status">
+                <p>Status: On Time</p>
+                <span id="status"></span>
+            </div>
+            <div className="additionalInformation">
+                <p>Additional Information: None</p>
+                <span id="subwayLine"></span>
+            </div>
+        </div>
+    )
+}
+
+export default SubwayLineComponent;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import SubwayLineComponent from '../component/SubwayLineComponent';
 
 const App = () => {
   const SubwayContainer = (props) => {
     // return <div className="SubwayContainer"></div>;
-    return <div className='SubwayContainer'>Subway Lines</div>;
+    return <div className='SubwayContainer'>Subway Lines <SubwayLineComponent/> </div>;
   };
   return (
     <div>
       <h1>Subway Alerts</h1>
       <SubwayContainer />
+      <div>
+      </div>
     </div>
   );
 };
